4d9d85bef4cda534610e0277186bb3a7e86d8909
HIT-YH <957404147@qq.com>
2024-03-31 10:55:53 +0800

    可以正常运行的版本代码20240331

--- hm-api/src/main/java/com/hmall/api/client/UserClient.java
+++ hm-api/src/main/java/com/hmall/api/client/UserClient.java
@@ -2,12 +2,12 @@ package com.hmall.api.client;
 
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient("user-service")
 public interface UserClient {
-    @DeleteMapping("/users/money/deduct")
+    @PutMapping("/users/money/deduct")
     void deductMoney(@RequestParam("pw") String pw,@RequestParam("amount") Integer amount);
 
 
